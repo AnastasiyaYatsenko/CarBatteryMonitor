cfd33dbf167a2ed5c669e0c1e2f233093cd04d3f
AnastasiyaYatsenko <62345631+AnastasiyaYatsenko@users.noreply.github.com>
2023-08-04 22:41:42 +0300

    update

--- CarBattery/CarBattery.ino
+++ CarBattery/CarBattery.ino
@@ -5,7 +5,14 @@
 #include <DallasTemperature.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
+
+
 #include <CircularBuffer.h>
+#include <Preferences.h>
+#include <WiFi.h>
+#include <time.h>
+
 
 #define Pin_i2c_scl 4
 #define Pin_i2c_sda 5
@@ -26,9 +33,64 @@ RTC_DATA_ATTR float busvoltage = 0;
 RTC_DATA_ATTR float current_mA = 0;
 RTC_DATA_ATTR float loadvoltage = 0;
 RTC_DATA_ATTR float power_mW = 0;
+RTC_DATA_ATTR int measurments_stored = 0;
 
 //CircularBuffer<float,288> measurmentStorage;
-CircularBuffer<float,6> measurmentStorage;
+//CircularBuffer<float,6> measurmentStorage;
+Preferences preferences;
+
+const char* ssid       = "OpenWRT";
+const char* password   = "1357924680";
+
+const char* ntpServer1 = "pool.ntp.org";
+const char* ntpServer2 = "time.nist.gov";
+const long  gmtOffset_sec = 7200;
+const int   daylightOffset_sec = 3600;
+const char* time_zone = "";
+
+time_t now;                         // this is the epoch
+tm myTimeInfo;
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("No time available (yet)");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+// Function that gets current epoch time
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return(0);
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  time(&now);
+  return now;
+}
+
+void synchroniseWith_NTP_Time() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("configTime uses ntpServer ");
+  Serial.println(ntpServer1);
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1);
+  Serial.print("synchronising time");
+  
+  while (myTimeInfo.tm_year + 1900 < 2000 ) {
+    time(&now);                       // read the current time
+    localtime_r(&now, &myTimeInfo);
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.print("\n time synchronsized \n");
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+}
 
 int playMelody(int melody[], int noteDuration){
   for (int i = 0; i < sizeof(melody) / sizeof(melody[0]); i++)
@@ -139,12 +201,94 @@ int drawVoltage(String voltage){
   return 0;
 }
 
-void writeMeasurment(float new_data){
-  if (measurmentStorage.isFull()){
-    measurmentStorage.shift();    
+//скорее всего минимальным будет первые в keys ключ, но на всякий лучше пока проверка
+void write_data(){
+  preferences.begin("battery", false);
+  unsigned long time_now = getTime();
+  char cstr[11];
+  itoa(time_now, cstr, 10);
+  preferences.putFloat(cstr, loadvoltage);
+//  float a = preferences.getFloat(cstr);
+  measurments_stored++;
+
+  if (preferences.isKey("keys")) {
+    size_t schLen = preferences.getBytes("keys", NULL, NULL);
+    char buffer[schLen];
+    preferences.getBytes("keys", buffer, schLen);
+    unsigned long *key_array = (unsigned long *) buffer;
+    int key_len = sizeof(buffer) / sizeof(unsigned long);
+    unsigned long new_key_array[key_len+1];
+    for (int i=0; i<key_len; i++){
+      new_key_array[i] = key_array[i];
+    }
+    new_key_array[key_len] = time_now;
+    preferences.putBytes("keys", &new_key_array, sizeof(new_key_array));
   }
-  measurmentStorage.push(new_data);
+  else {
+    measurments_stored=1;
+    unsigned long key_array[] = {time_now};
+    preferences.putBytes("keys", &key_array, sizeof(key_array));
+  }
+
+
+//  if (measurments_stored>288){
+  if (measurments_stored>5){
+    unsigned long min_key = 4294967295;
+    int min_i = 0;
+    size_t schLen = preferences.getBytes("keys", NULL, NULL);
+    char buffer[schLen];
+    preferences.getBytes("keys", buffer, schLen);
+    unsigned long *key_array = (unsigned long *) buffer;
+    int key_len = sizeof(buffer) / sizeof(unsigned long);
+    for (int i=0; i<key_len; i++){
+      unsigned long current_key = key_array[i];
+      if (current_key<min_key){
+        min_key = current_key;
+        min_i = i;
+      }
+    }
+    char remove_cstr[11];
+    itoa(min_key, remove_cstr, 10);
+    preferences.remove(remove_cstr);
+
+    unsigned long new_key_array[key_len-1];
+    for (int i=0; i<min_i; i++){
+      new_key_array[i] = key_array[i];
+    }
+    for (int i=min_i; i<key_len-1; i++){
+      new_key_array[i] = key_array[i+1];
+    }
+    preferences.putBytes("keys", &new_key_array, sizeof(new_key_array));
+    measurments_stored--;    
+  }  
+  preferences.end();
 }
+
+void printMemory(){
+  preferences.begin("battery", false);
+  if (preferences.isKey("keys")) {
+    size_t schLen = preferences.getBytes("keys", NULL, NULL);
+    char buffer[schLen];
+    preferences.getBytes("keys", buffer, schLen);
+    unsigned long *key_array = (unsigned long *) buffer;
+    int key_len = sizeof(buffer) / sizeof(unsigned long);
+    Serial.printf("key_len=%d : ",key_len);
+    for (int i=0; i<key_len; i++){
+      char cstr[11];
+      itoa(key_array[i], cstr, 10);
+      Serial.print(preferences.getFloat(cstr));
+      Serial.print(" ");
+    }
+  }
+  preferences.end();
+}
+
+//void writeMeasurment(float new_data){
+//  if (measurmentStorage.isFull()){
+//    measurmentStorage.shift();    
+//  }
+//  measurmentStorage.push(new_data);
+//}
 
 void setup(){
   Serial.begin(115200);
@@ -154,6 +298,8 @@ void setup(){
   pinMode(Pin_LedRed, OUTPUT);
   pinMode(Pin_LedYellow, OUTPUT);
   pinMode(Pin_FreeButton, INPUT);
+
+  synchroniseWith_NTP_Time();
 
   ds18.begin();
   Wire.begin(Pin_i2c_sda, Pin_i2c_scl);
@@ -181,24 +327,15 @@ void loop(){
   esp_sleep_enable_timer_wakeup(30000000); //30sec, 5min = 300000000
   
   
-//  for (int percentage = 0; percentage <= 100; percentage++)
-//  {
-//    drawLoadingBar(percentage);
-//    delay(10);
-//  }
-  
   esp_sleep_wakeup_cause_t wakeup_reason;
   wakeup_reason = esp_sleep_get_wakeup_cause();
-  Serial.println(wakeup_reason);
-  Serial.println(ESP_SLEEP_WAKEUP_EXT0);
-  Serial.println(ESP_SLEEP_WAKEUP_EXT1);
   if (wakeup_reason == 7){
     displayInfo();
   }
   else {
-//    playSuccessMelody();
     getIna219Data();
-    writeMeasurment(loadvoltage);
+    write_data();
+    printMemory();
 //    for (byte i = 0; i < measurmentStorage.size() - 1; i++) {
 //      Serial.print(measurmentStorage[i]);
 //      Serial.print(" ");
